3709804016649d4b971183ff8f4b5667c105a225
Argent <10303636+ominocutherium@users.noreply.github.com>
2021-09-11 11:40:24 -0600

    Write some install tests (bug exposed)

--- automation/tests/test_install.py
+++ automation/tests/test_install.py
@@ -8,9 +8,21 @@ from unittest import mock
 import automation.install
 
 class InstallTestCase(unittest.TestCase):
-    @unittest.skip("Pending test implementation.")
-    def test_get_role_from_config(self):
-        self.assertTrue(False)
+    @mock.patch('automation.install.os.path.exists')
+    @mock.patch('automation.install.os.path.join')
+    def test_get_role_from_config(self,path_join_mock,path_exists_mock):
+        m = mock.mock_open(read_data='booger booger\ndocs_defaults booger.yaml\nrole developer\n')
+        ir = automation.install.InstallRole()
+        with mock.patch('automation.install.open',m):
+            ir.get_role_from_working_tree_config()
+            self.assertEqual(ir.role,"developer")
+        m2 = mock.mock_open(read_data='booger booger\nbooger booger\n')
+        ir2 = automation.install.InstallRole()
+        with mock.patch('automation.install.open',m2):
+            ir2.get_role_from_working_tree_config()
+            self.assertEqual(ir.role,"fresh")
+        path_exists_mock.assert_called()
+        path_join_mock.assert_called()
 
     @unittest.skip("Pending test implementation.")
     def test_write_role(self):
@@ -28,7 +40,10 @@ class InstallTestCase(unittest.TestCase):
     def test_install(self):
         self.assertTrue(False)
 
-    @unittest.skip("Pending test implementation.")
-    def test_run(self):
-        self.assertTrue(False)
+    @mock.patch('automation.install.InstallRole')
+    def test_run(self,ir_class_mock):
+        ir_class_mock.return_value = mock.MagicMock()
+        automation.install.run()
+        ir_class_mock.return_value.get_role_from_working_tree_config.assert_called()
+        ir_class_mock.return_value.install.assert_called()
 
