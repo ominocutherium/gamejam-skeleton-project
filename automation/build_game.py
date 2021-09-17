9c20daf7b56d166d802028141095c5e0bdece2e7
Argent <10303636+ominocutherium@users.noreply.github.com>
2021-09-17 11:29:34 -0600

    port over code from other game jam script to this project

--- automation/build_game.py
+++ automation/build_game.py
@@ -2,5 +2,45 @@
 
 # Part of ominocutherium's godot gamejam skeleton project.
 
-def run():
-    pass
+import os
+import sys
+import subprocess
+
+class BuildsToBuild:
+    builds_by_platform = []
+
+    def read_build_info_from_config(self):
+        if os.path.exists(os.path.join('automation','config.txt')):
+            with open(os.path.join('automation','config.txt')) as config_file:
+                for line in config_file:
+                    data = line.split()
+                    if len(data) > 3:
+                        if data[0] = "build_platform":
+                            platform = BuildInfo()
+                            platform.build_dir = data[1]
+                            platform.platform_template_name = data[2]
+                            platform.itch_channel_name = data[3]
+
+    def create_builds(self) -> int:
+        for platform in builds_by_platform:
+            sp_state = platform.create_build()
+            if sp_state.return_code != 0:
+                return sp_state.return_code
+        return 0
+
+class BuildInfo:
+    build_dir = ""
+    platform_template_name = ""
+    itch_channel_name = ""
+
+    def create_build(self):
+        sp_state = subprocess.run(["godot","--path","game/","--export",self.platform_template_name])
+        return sp_state
+
+def run() -> list:
+    builds = BuildsToBuild()
+    builds.read_build_info_from_config()
+    builds_status = builds.create_builds():
+    if builds_status != 0:
+        sys.exit(builds_status)
+    return builds.builds_by_platform
--- automation/upload_builds_to_itch.py
+++ automation/upload_builds_to_itch.py
@@ -2,7 +2,38 @@
 
 # Part of ominocutherium's godot gamejam skeleton project.
 
+import subprocess
 
-def run():
-    pass
+class BuildsToUpload:
+    builds_by_platform = []
+    itch_user : str = ""
+    game_name : str = ""
+
+    def __init__(self,list_of_build_info:list=[]):
+        self.builds_by_platform = list_of_build_info
+
+    def read_itch_user_info_from_config(self):
+        if os.path.exists(os.path.join('automation','config.txt')):
+            with open(os.path.join('automation','config.txt')) as config_file: 
+                for line in config_file:
+                    if len(line.split()) > 2:
+                        if line.split()[0] == "itch_config":
+                            self.itch_user = line.split()[1]
+                            self.game_name = line.split()[2]
+   
+    def push_builds(self) -> int:
+        if self.itch_user == "" or self.game_name == "":
+            return 0
+        for build_info in builds_by_platform:
+            sp_status = subprocess.run([
+                    "butler",
+                    "push",
+                    build_info.build_dir,
+                    self.itch_user + "/" + self.game_name + ":{0}".format(build_info.itch_channel_name)
+                    ])
+            if sp_status.return_code != 0:
+                return sp_status.return_code
+        return 0
+
+# No run function because requires data from another module ('automation/build_game') to work.
 
